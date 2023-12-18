c6e0fd02d32ba3ea4b0c3825c0d49238446e1c09
Pedro Henrique <pedrohenriquesj.pro@gmail.com>
2023-12-18 12:05:13 -0300

    ADD: Speedinsights

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
 import { AppRoutes } from "./routes/routes.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
+      <SpeedInsights />
       <AppRoutes />
     </ThemeProvider>
   </React.StrictMode>
